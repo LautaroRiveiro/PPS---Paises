f417314fd13a50ab07df82da9ea6af4fae80b539
lautaroriveiro <lautaroriveiro91@gmail.com>
2016-09-10 09:24:51 -0300

    Recuperar datos de servidor de países

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,19 +2,21 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('ChatsCtrl', function($scope, $http, Chats) {
+    $scope.chats = Chats.all();
+    $scope.remove = function(chat) {
+      Chats.remove(chat);
+    };
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+    //Llamo al servidor de los países y almaceno la información en $scope.paises
+    $http.get('https://restcountries.eu/rest/v1/region/americas')
+        .then(function(data){
+          $scope.paises = data.data;
+        },
+        function(error){
+          alert("Error: "+error);
+    });
+
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
